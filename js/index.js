9b73a7e47819cf795e419429561d5808c507d242
Elena <e.mandrovska@gmail.com>
2021-10-08 15:12:28 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,35 @@ const refs = {
     list: document.querySelector('#list'),
 }
 
+function getTodos() {
+    const savedTodos = JSON.parse(localStorage.getItem('todos'))
+    if(savedTodos) {
+        savedTodos.map(el => {
+            const itemEl = document.createElement('li');
+                itemEl.classList.add('todo-item');
+                itemEl.style.display = 'flex';
+
+            const itemName = document.createElement('p');
+                itemName.classList.add('todo-text');
+                itemName.textContent = el;
+                itemEl.appendChild(itemName);
+
+            const itemBtnDone = document.createElement('button');
+                itemBtnDone.classList.add('done-btn');
+                itemBtnDone.textContent = 'done';
+                itemEl.appendChild(itemBtnDone);
+
+            const itemBtnRmv = document.createElement('button');
+                itemBtnRmv.classList.add('rmv-btn');
+                itemBtnRmv.textContent = 'remove';
+                itemEl.appendChild(itemBtnRmv);
+                refs.list.appendChild(itemEl);
+                onClickBtnRmv(itemBtnRmv);
+                onClickBtnDone(itemBtnDone);
+        })
+    }
+}
+getTodos()
 function createItem(e) {
     if (refs.input.value.length > 0){
     const itemEl = document.createElement('li');
@@ -27,12 +56,23 @@ function createItem(e) {
         itemEl.appendChild(itemBtnRmv);
 
     refs.list.appendChild(itemEl);
+    updateStorage()
         refs.input.value = '';
-        onClickBtnRmv(itemBtnRmv)
-        onClickBtnDone(itemBtnDone)
+        onClickBtnRmv(itemBtnRmv);
+        onClickBtnDone(itemBtnDone);
+
     }
 }
+function updateStorage() {
+    const todosData = [];
+    for (let element of list.querySelectorAll('p')) {
+        todosData.push(element.textContent);
+    }
+    localStorage['todos'] = JSON.stringify(todosData);
+  }
+
 function clearList(e) {
+    localStorage.clear()
     refs.list.innerHTML = '';
 }
 refs.input.addEventListener("keyup", (e) => {
@@ -54,7 +94,6 @@ function onClickBtnDone(el) {
         }
     })
 }
-
 
 refs.createBtn.addEventListener('click', createItem);
 refs.clearBtn.addEventListener('click', clearList);
